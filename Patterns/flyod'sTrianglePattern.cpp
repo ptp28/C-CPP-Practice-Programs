14a4d09c7951835dc4a6ed52d003bb98b222e403
Parth Parikh <parthparikh28@gmail.com>
2021-04-17 18:14:59 +0530

    Renamed program

--- Patterns/flyod'sTrianglePattern.cpp
+++ Patterns/flyod'sTrianglePattern.cpp
@@ -1,8 +1,9 @@
-/* Write a program to display the following pattern
+/* Write a program to display the following pattern (Flyod's Triangle)
 1
 2   3
 4   5   6
 7   8   9   10
+11  12  13  14  15 
 */
 
 #include<iostream>
